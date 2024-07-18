67d3429f20bb941c1d10138cba7b9d3bffee5dc9
kasarkonic <maris.dirveik@gmail.com>
2024-07-18 14:49:00 +0300

    add arduino files

--- Arduino/Modbus/ModbusRTUSlaveExample/ModbusRTUSlaveExample.ino
+++ Arduino/Modbus/ModbusRTUSlaveExample/ModbusRTUSlaveExample.ino
@@ -23,7 +23,7 @@
   !WARNING
   When connecting boards using UART, as described in the circuit below, the logic level voltages must match (5V or 3.3V).
   If they do not, use a logic level converter, otherwise your 3.3V board could be damaged.
-  
+
   Circuit:
   - The center pin of a potentiometer to pin A0, and the outside pins of the potentiometer to your board's logic level voltage (5V or 3.3V) and GND
   - The center pin of a potentiometer to pin A1, and the outside pins of the potentiometer to your board's logic level voltage (5V or 3.3V) and GND
@@ -40,50 +40,40 @@
 
   !NOTE
   Both boards will also need power.
-  
+
   Created: 2023-07-22
   By: C. M. Bulliner
   Last Modified: 2024-01-27
   By: C. M. Bulliner
-  
+
 */
 
-#if __AVR__
-  // Uncomment the following line if you want to use SoftwareSerial on pins 10 and ll; note this only works on AVR boards.
-  //# define USE_SOFTWARE_SERIAL
-#endif
 
-#if defined USE_SOFTWARE_SERIAL
-  #include <SoftwareSerial.h>
-#endif
 
-#include <ModbusRTUSlave.h>
+#include <SoftwareSerial.h>
+//#include <ModbusRTUSlave.h>
 
 const byte potPins[2] = {A0, A1};
-#if defined ESP32 || (defined ARDUINO_NANO_RP2040_CONNECT && !defined ARDUINO_ARCH_MBED)
-const byte buttonPins[2] = {D2, D3};
-const byte ledPins[4] = {D5, D6, D7, D8};
-const byte dePin = D13;
-#else
+
 const byte buttonPins[2] = {2, 3};
 const byte ledPins[4] = {5, 6, 7, 8};
 const byte dePin = 13;
-#endif
 
-#if defined USE_SOFTWARE_SERIAL
-  const byte rxPin = 10;
-  const byte txPin = 11;
-  SoftwareSerial mySerial(rxPin, txPin);
-  ModbusRTUSlave modbus(mySerial, dePin);  // serial port, driver enable pin for rs-485
-#else
-  #if (defined __AVR_ATmega328P__ || defined __AVR_ATmega168__ || defined __AVR_ATmega1280__ || defined __AVR_ATmega2560__)
-    ModbusRTUSlave modbus(Serial, dePin);  // serial port, driver enable pin for rs-485
-  #elif defined ESP32
-    ModbusRTUSlave modbus(Serial0, dePin); // serial port, driver enable pin for rs-485
-  #else
-    ModbusRTUSlave modbus(Serial1, dePin); // serial port, driver enable pin for rs-485
-  #endif
-#endif
+
+
+const byte rxPin = 10;
+const byte txPin = 11;
+
+
+
+//?SoftwareSerial mySerial(rxPin, txPin);
+SoftwareSerial mySerial(rxPin, txPin);
+
+//SoftwareSerial mySerialM(rxPin+2, txPin+2);
+
+//ModbusRTUSlave modbus(mySerialM, dePin);  // serial port, driver enable pin for rs-485
+
+
 
 bool coils[2];
 bool discreteInputs[2];
@@ -91,9 +81,15 @@ uint16_t holdingRegisters[2];
 uint16_t inputRegisters[2];
 
 void setup() {
-  #if defined ESP32
-    analogReadResolution(10);
-  #endif
+  mySerial.begin(9600);
+ // mySerial.print("0");
+
+  
+  Serial.begin(115200);
+  Serial.println("Modbus START");
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+
 
   pinMode(potPins[0], INPUT);
   pinMode(potPins[1], INPUT);
@@ -104,12 +100,12 @@ void setup() {
   pinMode(ledPins[2], OUTPUT);
   pinMode(ledPins[3], OUTPUT);
 
-  modbus.configureCoils(coils, 2);                       // bool array of coil values, number of coils
-  modbus.configureDiscreteInputs(discreteInputs, 2);     // bool array of discrete input values, number of discrete inputs
-  modbus.configureHoldingRegisters(holdingRegisters, 2); // unsigned 16 bit integer array of holding register values, number of holding registers
-  modbus.configureInputRegisters(inputRegisters, 2);     // unsigned 16 bit integer array of input register values, number of input registers
+ // modbus.configureCoils(coils, 2);                       // bool array of coil values, number of coils
+ // modbus.configureDiscreteInputs(discreteInputs, 2);     // bool array of discrete input values, number of discrete inputs
+ // modbus.configureHoldingRegisters(holdingRegisters, 2); // unsigned 16 bit integer array of holding register values, number of holding registers
+ // modbus.configureInputRegisters(inputRegisters, 2);     // unsigned 16 bit integer array of input register values, number of input registers
 
-  modbus.begin(1, 38400);
+ // modbus.begin(1, 38400);
 }
 
 void loop() {
@@ -117,9 +113,13 @@ void loop() {
   inputRegisters[1] = map(analogRead(potPins[1]), 0, 1023, 0, 255);
   discreteInputs[0] = !digitalRead(buttonPins[0]);
   discreteInputs[1] = !digitalRead(buttonPins[1]);
-  
-  modbus.poll();
 
+ // modbus.poll();
+ // mySerial.println("0");
+ Serial.println("Modbus START");
+ mySerial.write(0x31);
+ 
+   delay(100);
   analogWrite(ledPins[0], holdingRegisters[0]);
   analogWrite(ledPins[1], holdingRegisters[1]);
   digitalWrite(ledPins[2], coils[0]);
